791d3725b4ef231c0fa00b45996daab32ad1f17a
Edgar Nazyrov <edgar.nazyrov@finch-xr.com>
2022-03-08 16:02:48 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "TensorFlowLite",
+    name: "TensorFlowLiteSwift",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TensorFlowLite",
+            name: "TensorFlowLiteSwift",
             targets: ["TensorFlowLiteSwift"]),
     ],
     dependencies: [
