c740ab0110d5eea4bcfa98ec7f0f57550482c4f7
Edgar Nazyrov <nazyrov01@gmail.com>
2022-03-15 16:09:32 +0500

    add dependency TensorFlowLiteC

--- Package.swift
+++ Package.swift
@@ -11,16 +11,16 @@ let package = Package(
             name: "TensorFlowLite",
             targets: ["TensorFlowLiteSwift"]),
     ],
+    dependencies: [
+        .package(name: "TensorFlowLiteC",
+                 url: "https://github.com/Edgarchuk/TensorFlowLiteC", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TensorFlowLiteSwift",
-            dependencies: ["TensorFlowLiteC", "TensorFlowLiteCCoreML", "TensorFlowLiteCMetal"],
+            dependencies: ["TensorFlowLiteC"],
             path: "Sources"),
-        
-        .binaryTarget(name: "TensorFlowLiteC", path: "TensorFlowLiteC.xcframework"),
-        .binaryTarget(name: "TensorFlowLiteCCoreML", path: "TensorFlowLiteCCoreML.xcframework"),
-        .binaryTarget(name: "TensorFlowLiteCMetal", path: "TensorFlowLiteCMetal.xcframework")
     ]
 )
